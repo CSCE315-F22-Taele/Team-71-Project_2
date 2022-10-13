c05ce3b62cd8b47b104026f65c841ea45026f394
r25oh <rohitshankar2001@tamu.edu>
2022-10-13 12:16:24 -0500

    removed comments

--- mainGUI.java
+++ mainGUI.java
@@ -1,11 +1,4 @@
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-//  w ww  .  j a  v a  2  s  .co  m
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+
 public class mainGUI {
     public static void main(String[]args){
         serverGUI sg = new serverGUI();
